6dc90e86a46eca0338448a48f3cf9e5b97f41662
cammyong <princewhobong@yahoo.com>
2023-12-19 00:30:32 +0200

    alb created

--- modules/acm/output.tf
+++ modules/acm/output.tf
@@ -1,7 +1,7 @@
 output "domain" {
-    value = "var.domain_name"
+    value = var.domain_name
 }
 
 output "certificate_arn" {
-    value = "aws_acm_certificate.acm_certificate.arn"
+    value = aws_acm_certificate.acm_certificate.arn
 }
---
+++ modules/alb/variable.tf
@@ -0,0 +1,6 @@
+variable "project_name" {}
+variable "alb_security_group" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "certificate_arn" {}
+variable "vpc_id" {}
